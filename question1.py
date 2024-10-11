fd24ee7a8e6d10198915bd8a0d940576c193d7e5
Cucurbitaceagile <kobsko15@gmail.com>
2024-10-12 01:16:36 +0200

    On a ajouté un paramètre n à la fonction pour spécifier la borne supérieure. La boucle for s'exécute maintenant de 1 à n inclus.

--- question1.py
+++ question1.py
@@ -1,6 +1,7 @@
-def affiche():
+def affiche(n):
+    
     result = ""
-    for i in range(1, 101):
+    for i in range(1, n + 1):
         if i % 15 == 0:
             result += "FrisBee"
         elif i % 3 == 0:
@@ -11,4 +12,4 @@ def affiche():
             result += str(i)
     print(result)
 
-affiche()
+affiche(15)
