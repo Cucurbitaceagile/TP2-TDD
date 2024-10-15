d2d9cfc75137baf7f1da022cd34151f908d79178
Cucurbitaceagile <kobsko15@gmail.com>
2024-10-15 11:08:31 +0200

    question2 de cryptage  b Fix cryptage espace avec pas

--- question2.py
+++ question2.py
@@ -1,30 +1,30 @@
 import string
 
-def crypt(message):
+def crypt(message, pas):
     caracteres = string.ascii_letters + string.punctuation + string.digits + " "
     result = ""
-    
+
     for char in message:
         if char == " ":
-            result += "!"
+            result += "!" 
         elif char in caracteres:
             index = caracteres.index(char)
-            result += caracteres[(index + 1) % len(caracteres)]
+            result += caracteres[(index + pas) % len(caracteres)]
         else:
             result += char  
-    return result
+    return result + str(pas)
 
-# Tests du TDD
+# Tests TDD
 def test_crypt():
-    assert crypt("abc") == "bcd", "Test 1 échoué"
-    assert crypt("ABC") == "BCD", "Test 2 échoué"
-    assert crypt("123") == "234", "Test 3 échoué"
-    assert crypt(" ") == "!", "Test 4 échoué"
+    assert crypt("abc", 1) == "bcd1", "Test 1 échoué"
+    assert crypt("ABC", 2) == "CDE2", "Test 2 échoué"
+    assert crypt("123", 3) == "4563", "Test 3 échoué"
+    assert crypt(" ", 4) == "!4", "Test 4 échoué" 
 
 test_crypt()
 
 # Exemple d'exécution
-print(crypt("abc"))  # Devrait afficher: bcd
-print(crypt("ABC"))  # Devrait afficher: BCD
-print(crypt("123"))  # Devrait afficher: 234
-print(crypt(" "))    # Devrait afficher: !
+print(crypt("abc", 1))  # Devrait afficher: bcd1
+print(crypt("ABC", 2))  # Devrait afficher: CDE2
+print(crypt("123", 3))  # Devrait afficher: 4563
+print(crypt(" ", 4))    # Devrait afficher: !4
