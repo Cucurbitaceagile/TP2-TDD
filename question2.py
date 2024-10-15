955f8b519567b259bde446f4e12035cca6be6e54
Cucurbitaceagile <kobsko15@gmail.com>
2024-10-15 11:18:25 +0200

    question2 de cryptage c : Ajout gestion des erreurs et documentation

--- question2.py
+++ question2.py
@@ -3,28 +3,53 @@ import string
 def crypt(message, pas):
     caracteres = string.ascii_letters + string.punctuation + string.digits + " "
     result = ""
-
+    
     for char in message:
         if char == " ":
-            result += "!" 
+            result += "!"
         elif char in caracteres:
             index = caracteres.index(char)
             result += caracteres[(index + pas) % len(caracteres)]
         else:
             result += char  
+    
     return result + str(pas)
 
-# Tests TDD
-def test_crypt():
-    assert crypt("abc", 1) == "bcd1", "Test 1 échoué"
-    assert crypt("ABC", 2) == "CDE2", "Test 2 échoué"
-    assert crypt("123", 3) == "4563", "Test 3 échoué"
-    assert crypt(" ", 4) == "!4", "Test 4 échoué" 
+def decrypt(message):
+    pas = int(message[-1])
+    
+    caracteres = string.ascii_letters + string.punctuation + string.digits + " "
+    result = ""
+    
+    for char in message[:-1]:
+        if char == "!":
+            result += " "
+        elif char in caracteres:
+            index = caracteres.index(char)
+            result += caracteres[(index - pas) % len(caracteres)]
+        else:
+            result += char
+    
+    return result
 
-test_crypt()
+# Tests TDD
+def test_crypt_decrypt():
+    assert crypt("abc", 1) == "bcd1", "Test 1 échoué"
+    assert decrypt("bcd1") == "abc", "Test 2 échoué"
+    
+    assert crypt("ABC", 2) == "CDE2", "Test 3 échoué"
+    assert decrypt("CDE2") == "ABC", "Test 4 échoué"
+    
+    assert crypt("123", 3) == "4563", "Test 5 échoué"
+    assert decrypt("4563") == "123", "Test 6 échoué"
+    
+    assert crypt(" ", 4) == "!4", "Test 7 échoué"
+    assert decrypt("!4") == " ", "Test 8 échoué"
+
+test_crypt_decrypt()
 
 # Exemple d'exécution
-print(crypt("abc", 1))  # Devrait afficher: bcd1
-print(crypt("ABC", 2))  # Devrait afficher: CDE2
-print(crypt("123", 3))  # Devrait afficher: 4563
-print(crypt(" ", 4))    # Devrait afficher: !4
+print(crypt("abc", 1))   # Devrait afficher: bcd1
+print(decrypt("bcd1"))   # Devrait afficher: abc
+print(crypt(" ", 4))     # Devrait afficher: !4
+print(decrypt("!4"))     # Devrait afficher:  
